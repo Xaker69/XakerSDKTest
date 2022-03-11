efcd63a820ed5e56d6510a9c8f36543cda21f37b
Xaker69 <max.xaker41@mail.ru>
2022-03-11 20:44:09 +0300

    add registration vc

---
+++ Module/Registration/RegistrationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationViewController.swift
+//  Pods-XakerSDKTest_Example
+//
+//  Created by Maxim Khrabryi on 11.03.2022.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ Module/Registration/View/RegistrationView.swift
@@ -0,0 +1,14 @@
+//
+//  RegistrationView.swift
+//  Pods-XakerSDKTest_Example
+//
+//  Created by Maxim Khrabryi on 11.03.2022.
+//
+
+import UIKit
+
+class RegistrationView: UIView {
+
+    
+
+}
